b0d777a9d9889778830dbeadb1a496c2aaf00b9d
sebasrpg1232 <sebasrpg1232@gmail.com>
2024-08-30 16:19:15 -0500

    [CHG] buttons components custom styles and bold function

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/Components/Buttons.css'
 
-export const LargeButton = ({ name, label = "", fn, isDisabled = false, styleType = "primary", icon = "" }) => {
+export const LargeButton = ({ name, label = "", fn, isDisabled = false, styleType = "primary", icon = "", boldText = false, customStyles = "" }) => {
     return (
         <div className='large_button_container'>
             <button
@@ -10,9 +10,10 @@ export const LargeButton = ({ name, label = "", fn, isDisabled = false, styleTyp
                 onClick={fn}
                 name={name}
                 disabled={isDisabled}
+                style={{ customStyles }}
             >
                 {icon && icon()}
-                <div className='text_button_container'>
+                <div className='text_button_container' style={boldText ? { fontWeight: 700 } : { fontWeight: 400 }} >
                     {label}
                 </div>
             </button>
@@ -20,7 +21,7 @@ export const LargeButton = ({ name, label = "", fn, isDisabled = false, styleTyp
     )
 }
 
-export const IconButton = ({ name, label = "", fn, isDisabled = false, icon = "", iconType = "primary" }) => {
+export const IconButton = ({ name, label = "", fn, isDisabled = false, icon = "", iconType = "primary", boldText = false, customStyles = "" }) => {
     const [circleColor, setCircleColor] = useState("none")
     const [textColor, setTextColor] = useState("#404245")
     const bgcolor = (iconType == "primary" ? "rgba(0, 0, 255, 0.3)" : iconType == "secondary" ? "rgba(255, 0, 0, 0.3)" : "rgba(0, 255, 0, 0.3)")
@@ -29,7 +30,7 @@ export const IconButton = ({ name, label = "", fn, isDisabled = false, icon = ""
         <div>
             <button
                 className={`button icon-button icon-${iconType}`}
-                style={{ color: textColor, fill: textColor }}
+                style={{ color: textColor, fill: textColor, customStyles }}
                 type="button"
                 onClick={fn}
                 name={name}
@@ -46,7 +47,7 @@ export const IconButton = ({ name, label = "", fn, isDisabled = false, icon = ""
                 <div className='circulo' style={{ background: circleColor }}>
                     {icon && icon()}
                 </div>
-                <div className='button_icon_text'>
+                <div className='button_icon_text' style={boldText ? { fontWeight: 700 } : { fontWeight: 400 }}>
                     {label && label}
                 </div>
 
@@ -57,10 +58,10 @@ export const IconButton = ({ name, label = "", fn, isDisabled = false, icon = ""
     )
 }
 
-export const LinkTextButton = ({ boldText = "", normalText = "", fn = () => console.log("this is a test") }) => {
+export const LinkTextButton = ({ boldText = "", normalText = "", fn = () => console.log("this is a test"), customStyles = "" }) => {
     return (
         <div>
-            <a onClick={fn}>
+            <a onClick={fn} style={{ customStyles }}>
                 {boldText && <span style={{ fontWeight: 'bold', color: "white" }}>{boldText + " "}</span>}
                 {normalText && <span>{normalText}</span>}
             </a>
