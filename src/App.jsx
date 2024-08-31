cd18795ea119e562c7b36195e2e3c036dd240d03
sebasrpg1232 <sebasrpg1232@gmail.com>
2024-08-30 19:52:22 -0500

    [CHG] buttons documentation, Router npm installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/Components/Buttons.css'
-
-export const LargeButton = ({ name, label = "", fn, isDisabled = false, styleType = "primary", icon = "", boldText = false, customStyles = {} }) => {
+/**
+ * Componente de botón largo personalizable.
+ *
+ * @param {object} props Propiedades del componente.
+ * @param {string} props.name Nombre del botón (usado para identificarlo y asignarlo a variables).
+ * @param {string} props.label Etiqueta del botón (texto a mostrar).
+ * @param {function(event)} [props.fn] Función a ejecutar cuando se hace clic en el botón. Recibe el evento del clic como parámetro.
+ * @param {boolean} [props.disabled=false] Indica si el botón debe estar deshabilitado (no se podrá hacer clic).
+ * @param {string} [props.styleType='primary'] Indica el estilo de los botones, (primary, secondary, tertiary).
+ * @param {object} [props.icon=''] Le agrega un boton al boton al lado izquierdo.
+ * @param {boolean} [props.boldText=false] Indica si el texto del boton es con negrilla.
+ * @param {string} [props.customStyles=''] Objeto con opciones de estilo adicionales para el botón.
+ *
+ * @returns {JSX.Element} Elemento JSX del componente.
+ */
+export const LargeButton = ({ name, label = "", fn, isDisabled = false, styleType = "primary", icon = "", boldText = false, customStyles = "" }) => {
     return (
         <div className='large_button_container'>
             <button
@@ -10,7 +24,7 @@ export const LargeButton = ({ name, label = "", fn, isDisabled = false, styleTyp
                 onClick={fn}
                 name={name}
                 disabled={isDisabled}
-                style={ customStyles }
+                style={customStyles}
             >
                 {icon && icon()}
                 <div className='text_button_container' style={boldText ? { fontWeight: 700 } : { fontWeight: 400 }} >
@@ -21,6 +35,21 @@ export const LargeButton = ({ name, label = "", fn, isDisabled = false, styleTyp
     )
 }
 
+/**
+ * Componente de botón icono personalizable.
+ *
+ * @param {object} props Propiedades del componente.
+ * @param {string} props.name Nombre del botón (usado para identificarlo y asignarlo a variables).
+ * @param {string} [props.label=""] Etiqueta del botón (texto a mostrar).
+ * @param {function(event)} [props.fn] Función a ejecutar cuando se hace clic en el botón. Recibe el evento del clic como parámetro.
+ * @param {boolean} [props.disabled=false] Indica si el botón debe estar deshabilitado (no se podrá hacer clic).
+ * @param {string} [props.styleType='primary'] Indica el estilo de los botones, (primary, secondary, tertiary).
+ * @param {object} [props.icon=''] Le agrega un boton al boton al lado izquierdo.
+ * @param {boolean} [props.boldText=false] Indica si el texto del boton es con negrilla.
+ * @param {string} [props.customStyles=''] Objeto con opciones de estilo adicionales para el botón.
+ *
+ * @returns {JSX.Element} Elemento JSX del componente.
+ */
 export const IconButton = ({ name, label = "", fn, isDisabled = false, icon = "", iconType = "primary", boldText = false, customStyles = "" }) => {
     const [circleColor, setCircleColor] = useState("none")
     const [textColor, setTextColor] = useState("#404245")
@@ -58,6 +87,18 @@ export const IconButton = ({ name, label = "", fn, isDisabled = false, icon = ""
     )
 }
 
+/**
+ * Componente de botón largo personalizable.
+ *
+ * @param {object} props Propiedades del componente.
+ * @param {string} props.name Nombre del botón (usado para identificarlo y asignarlo a variables).
+ * @param {string} props.boldText Indica el texto en negrilla a mostrar (texto a mostrar).
+ * @param {string} props.normalText Indica el texto a mostrar (texto a mostrar).
+ * @param {function(event)} [props.fn] Función a ejecutar cuando se hace clic en el botón. Recibe el evento del clic como parámetro.
+ * @param {string} [props.customStyles=''] Objeto con opciones de estilo adicionales para el botón.
+ *
+ * @returns {JSX.Element} Elemento JSX del componente.
+ */
 export const LinkTextButton = ({ boldText = "", normalText = "", fn = () => console.log("this is a test"), customStyles = "" }) => {
     return (
         <div>
