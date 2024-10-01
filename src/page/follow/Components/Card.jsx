9972edd7862c4cb551e03f03c5048e00b3c755a8
sebasrpg1232 <sebasrpg1232@gmail.com>
2024-10-01 09:24:32 -0500

    [ADD] component FollowsCard

---
+++ src/page/follow/Components/Card.jsx
@@ -0,0 +1,36 @@
+import imagen from "/images/fqIKBO06_400x400.jpg"
+import "./Styles.css"
+import { ThreeDotsIconSVG } from "../../../components/Images";
+import { IconButton, LargeButton } from "../../../components/Buttons";
+import { ProfileIcon } from "../../../components/ProfileIcon";
+const Card = ({ image = imagen, username = "carius", usertag = "@carlitos99", isFollowed = true, isFollowingUser = true }) => {
+    return (
+        <>
+            <div id="card">
+                <div id="card_data_container">
+                    <ProfileIcon imageUrl={imagen} />
+                    <div className="card_text_container">
+                        <span className="card_username_text">{username}</span>
+                        <span>
+                            {usertag}
+                            {isFollowingUser &&
+                                <span id="card_followed_state">
+                                    Te sigue
+                                </span>
+                            }
+                        </span>
+
+                    </div>
+                </div>
+
+                <div id="card_actions">
+                    <LargeButton label={isFollowed ? "Siguiendo" : "Seguir"} styleType="secondary" customClass="card_largebutton_text" />
+                    <IconButton icon={() => ThreeDotsIconSVG({ width: "26.250px", height: "26.250px" })} />
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default Card;
