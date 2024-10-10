8079b18b9a7ac0dc7465956e3cf5135c2856163e
sebasrpg1232 <sebasrpg1232@gmail.com>
2024-10-09 22:08:36 -0500

    [CHG] Profile icon size bugfixed, custom activator added

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import "../styles/components/ProfileIcon.css"
 
-export const ProfileIcon = ({imageUrl}) => {
+export const ProfileIcon = ({ imageUrl, large = false }) => {
 	return (
-		<div className="profile-image-container">
+		<div className="profile-image-container" style={large ? { width: "140px", height: "140px" } : { width: "38px", height: "38px" }}>
 			<img src={imageUrl} alt="" className="profile-image" />
 		</div>
 	);
