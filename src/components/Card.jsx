cd9b8653a32b2c16107d5e5695f459bb84969587
juanjot08 <juan.tellez@correo.tdea.edu.co>
2024-11-08 08:34:35 -0500

    [CHG] funcionalidad Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,43 +1,52 @@
-import imagen from "../../public/images/fqIKBO06_400x400.jpg";
+import imagen from "/images/fqIKBO06_400x400.jpg";
 import "../styles/Components/CardStyles.css";
-import { ThreeDotsIconSVG } from "./Images.jsx";
-import { IconButton, LargeButton } from "./Buttons";
+import { ThreeDotsIconSVG } from "./Images";
+import { FollowButton, IconButton } from "./Buttons";
 import { ProfileIcon } from "./ProfileIcon";
-const Card = ({
+import { useNavigate, useParams } from "react-router-dom";
+
+export const Card = ({
   image = imagen,
   username = "carius",
   usertag = "@carlitos99",
-  isFollowed = true,
+  currentId,
+  targetId,
   isFollowingUser = false,
-  customCard = false
+  reFetchFunction = () => {},
 }) => {
+  const { userName } = useParams();
+  const navigate = useNavigate();
+
   return (
-    <div id={!customCard ? " card" : "custom_card_container"}>
-      <div id={"card_data_container"}>
+    <div id="card">
+      <div id="card_data_container">
         <ProfileIcon imageUrl={image} large={false} />
         <div className="card_text_container">
-          <span className="card_username_text">{username}</span>
+          <span
+            className="card_username_text"
+            onClick={() => navigate("/" + userName)}
+          >
+            {username}
+          </span>
           <span>
-            {usertag}
+            @{usertag}
             {isFollowingUser && <span id="card_followed_state">Te sigue</span>}
           </span>
         </div>
       </div>
 
       <div id="card_actions">
-        <LargeButton
-          label={isFollowed ? "Siguiendo" : "Seguir"}
-          styleType="secondary"
-          customClass="card_largebutton_text"
+        <FollowButton
+          currentUser={currentId}
+          targetUser={targetId}
+          reFetchFuncion={() => reFetchFunction()}
         />
-        {!customCard && <IconButton
+        <IconButton
           icon={() =>
             ThreeDotsIconSVG({ width: "26.250px", height: "26.250px" })
           }
-        />}
+        />
       </div>
     </div>
   );
 };
-
-export default Card;
